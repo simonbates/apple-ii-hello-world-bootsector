c2f4793f18979b77a1124d927f4eb1b5f4cbe6de
Simon Bates <simon@bitstructures.com>
2018-03-01 08:21:43 -0500

    Update comments

--- hello.s
+++ hello.s
@@ -1,16 +1,19 @@
-        * = $0800       ; The Disk II ROM will load us into $0800
+; The Disk II controller card will load us into $0800
+* = $0800
+
+COUT = $fded
 
         .byt $01        ; Load 1 sector at boot
 
-        ldx #$00
+        ldx #$00        ; Print "HELLO WORLD"
 loop    lda message,x
         beq done
-        ora #$80        ; Display regular letters rather than flashing
-        jsr $fded       ; COUT
+        ora #$80        ; Use regular letters rather than flashing
+        jsr COUT
         inx
         bne loop
 done
-        jmp done        ; TODO wait for keypress and boot into BASIC
+        jmp done        ; TODO Wait for keypress, continue boot into BASIC
 message
         .asc "HELLO WORLD"
         .byt $00
